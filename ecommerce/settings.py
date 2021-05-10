3d23ef762d3413828980d313c59c543363ad7659
ninchy0 <minmar666@gmail.com>
2021-05-10 18:30:43 +0545

    .

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -115,11 +115,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_URL = 'media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- home/views.py
+++ home/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import *
+from django.views.generic import View
 
 # Create your views here.
+class BaseView(View):
+    views = {}
+
+class HomeView(BaseView):
+    def get(self, request):
+        self.views['category'] = Category.objects.filter(status = 'active')
+        self.views['sliders'] = Slider.objects.filter(status = 'active')
+        self.views['ads'] = Ad.objects.all()
+        self.views['brand'] = Brand.objects.filter(status = 'active')
+        self.views['hots'] = Item.objects.filter(status = 'active', label = 'hot')
+        self.views['new'] = Item.objects.filter(status='active', label='new')
+
+        return render(request, 'index.html', self.views)
