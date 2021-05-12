28e6317ed43826768fff378e659e5f65fce1c8c8
ninchy0 <minmar666@gmail.com>
2021-05-12 18:32:18 +0545

    Update v1.01

--- home/views.py
+++ home/views.py
@@ -5,14 +5,23 @@ from django.views.generic import View
 # Create your views here.
 class BaseView(View):
     views = {}
+    views['category'] = Category.objects.filter(status = 'active')
 
 class HomeView(BaseView):
     def get(self, request):
-        self.views['category'] = Category.objects.filter(status = 'active')
         self.views['sliders'] = Slider.objects.filter(status = 'active')
         self.views['ads'] = Ad.objects.all()
-        self.views['brand'] = Brand.objects.filter(status = 'active')
+        self.views['brands'] = Brand.objects.filter(status = 'active')
         self.views['hots'] = Item.objects.filter(status = 'active', label = 'hot')
-        self.views['new'] = Item.objects.filter(status='active', label='new')
+        self.views['news'] = Item.objects.filter(status='active', label='new')
 
         return render(request, 'index.html', self.views)
+
+class CategoryItemView(BaseView):
+    def get(self, request, slug):
+        category_id = Category.objects.get(slug=slug).id
+        self.views['cat_items'] = Item.objects.filter(category = category_id)
+        return render(request, 'category.html', self.views)
+
+
+# BRANDS KO ADAR MA VIEW GARNE
